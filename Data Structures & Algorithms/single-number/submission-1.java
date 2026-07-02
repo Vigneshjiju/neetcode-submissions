afcc28891c9ae33c0edd5128b0b90446319bf571
Vignesh BG <160627021+Vigneshjiju@users.noreply.github.com>
2026-07-02 23:01:11 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        /*HashSet<Integer> set=new HashSet<>();
+        for(int num:nums)
+        {
+            if(set.contains(num))
+            {
+                set.remove(num);
+            }
+            else
+            {
+                set.add(num);
+            }
+        }
+        return set.iterator().next();
+    */
+    int ans=0;
+    for(int num:nums)
+    {
+        ans=ans^num;
+    }
+    return ans;
+    }
+}
